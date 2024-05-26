9d8a62d555964843a4965b2e3ead9e9d8fb04475
MuhiburR <90928378+MuhiburR@users.noreply.github.com>
2024-05-25 23:14:55 -0400

    Update WordChecker.java

--- FRQ Solutions/Q3/WordChecker.java
+++ FRQ Solutions/Q3/WordChecker.java
@@ -25,9 +25,12 @@ public class WordChecker{
 	public ArrayList<String> createList(String target){
 		ArrayList<String> result = new ArrayList<String>();
 			for(int i = 0; i < wordList.size(); i++){
-				
-			}
-		
+				String word = wordList.get(i);
+					if(word.indexOf(target) != -1){
+						result.add(word.substring(word.indexOf(target)+target.length()));
+					}
+				}
+		return result;
 	}
 	
 }
